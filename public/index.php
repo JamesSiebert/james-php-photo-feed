e16157a803913a195b4621beeca590badc899e41
James Siebert <js@media360.com.au>
2022-02-03 17:11:24 +1000

    Basic front end

--- public/index.php
+++ public/index.php
@@ -1,1 +1,53 @@
-INDEX
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Global Photo Feed</title>
+</head>
+<body>
+
+<!--Simple Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand">Global Photo Feed</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./index.php">View Feed</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="upload_form.php">Post a Photo</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="mx-auto" style="width: 400px;">
+        <div class="card border-primary mb-3 mt-5">
+            <div class="card-body text-primary">
+                <h5 class="card-title mb-2">Public Photo Feed</h5>
+                <div class="mt-3 alert alert-info" role="alert">
+                    Share your favorite photos with people all over the world!
+                </div>
+                <div>
+                    <div id="post-list"></div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
--- public/upload_form.php
+++ public/upload_form.php
@@ -1,1 +1,55 @@
-UPLOAD FORM
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Global Photo Feed</title>
+</head>
+<body>
+
+<!--Simple Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand">Global Photo Feed</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="./index.php">View Feed</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="upload_form.php">Post a Photo</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="mx-auto" style="width: 400px;">
+        <div class="card border-primary mb-3 mt-5">
+            <div class="card-body text-primary">
+                <h5 class="card-title mb-2">Create a post</h5>
+                <div class="mt-3">
+
+
+
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
